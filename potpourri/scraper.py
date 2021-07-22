4199a6c56238e04b82f09cf6912fde27ca6381e8
Chubak <chubakbidpaa@gmail.com>
2021-07-23 02:39:40 +0430

    added pandas df

--- potpourri/scraper.py
+++ potpourri/scraper.py
@@ -6,11 +6,11 @@ from .scripts.get_urls import search_web
 from .scripts.unqlite_interface import store_in_db
 from .scripts.page_speed import get_page_speed_multiple, get_page_speed
 from .scripts.page_rank import get_page_ranks
-
+from .scripts.utils import *
 from random_word import RandomWords
 from pprint import pprint
-from .scripts.utils import *
 import time
+import pandas as pd
 
 class Scraper:
     def __init__(self, min_rake_length=2, max_rake_length=4):
@@ -300,6 +300,104 @@ class Scraper:
             ret[res_id] = self.request_own_page_rank(res_id)
 
         return ret
+
+    def make_pandas_df(self, keys_or_urls):
+        urls = []
+        ids = []
+        htmls = []
+        bodies = []
+        h1s = []
+        h2s = []
+        h3s = []
+        h4s = []
+        h5s = []
+        h6s = []
+        hrefs = []
+        internal_links = []
+        external_links = []
+        meta_descs = []
+        meta_authors = []
+        titles = []
+        classics = []
+        meta_keywords = []
+        image_alts = []
+        bolds = []
+        italics = []
+        lists = []
+        strongs = []
+        custom_tags = []
+        custom_attrs = []
+        parsed_urls = []
+        page_speeds = []
+        page_ranks = []
+
+        for res_id in keys_or_urls:
+            if match_url(res_id):
+                res_id = self.ids_sites[get_best_match(res_id, self.ids_sites)]
+
+            urls.append(self.get_address(res_id))
+            ids.append(res_id)
+            htmls.append(self.get_html(res_id))
+            titles.append(self.get_title(res_id))
+            bodies.append(self.get_body(res_id))
+            h1s.append(self.get_h1(res_id))
+            h2s.append(self.get_h2(res_id))
+            h3s.append(self.get_h3(res_id))
+            h4s.append(self.get_h4(res_id))
+            h5s.append(self.get_h5(res_id))
+            h6s.append(self.get_h6(res_id))
+            hrefs.append(self.get_hrefs(res_id))
+            internal_links.append(self.get_internal_urls(res_id))
+            external_links.append(self.get_external_urls(res_id))
+            meta_descs.append(self.get_meta_desc(res_id))
+            meta_authors.append(self.get_meta_author(res_id))
+            meta_keywords.append(self.get_meta_keywords(res_id))
+            image_alts.append(self.get_image_alts(res_id))
+            bolds.append(self.get_bolds(res_id))
+            italics.append(self.get_italics(res_id))
+            strongs.append(self.get_strongs(res_id))
+            custom_tags.append(self.get_custom_tags(res_id))
+            custom_attrs.append(self.get_custom_attrs(res_id))
+            lists.append(self.get_lists(res_id))
+            classics.append(self.get_classics(res_id))
+            parsed_urls.append(self.get_url_parsed(res_id))
+            page_speeds.append(self.get_page_speed(res_id))
+            page_ranks.append(self.get_page_ranks(res_id))
+
+        df = pd.DataFrame.from_dict({"id": ids, 
+                                        "url": urls,
+                                        "parsed_url": parsed_urls,
+                                        "page_speed": page_speeds,
+                                        "page_rank": page_ranks,                                        
+                                        "html": htmls,
+                                        "title": titles,
+                                        "body": bodies,
+                                        "classic": classics,
+                                        "h1": h1s,
+                                        "h2": h2s,
+                                        "h3": h3s,
+                                        "h4": h4s,
+                                        "h5": h5s,
+                                        "h6": h6s,
+                                        "href": hrefs,
+                                        "internal_links": internal_links,
+                                        "external_links": external_links,
+                                        "meta_desc": meta_descs,
+                                        "meta_keywords": meta_keywords,
+                                        "meta_author": meta_authors,
+                                        "image_alts": image_alts,
+                                        "bolds": bolds,
+                                        "italics": italics,
+                                        "strongs": strongs,
+                                        "lists": lists,
+                                        "custom_tags": custom_tags,
+                                        "custom_attrs": custom_attrs})
+
+        return df
+        
+            
+
+
 
         
         
