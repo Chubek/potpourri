188b75099962f0c0aa1e0f5897afecd10f7d2278
Chubak <chubakbidpaa@gmail.com>
2021-07-24 02:26:15 +0430

    fixed arg names

--- potpourri/high_level.py
+++ potpourri/high_level.py
@@ -5,7 +5,7 @@ def search_and_scrape_single(scraper, psearch, keyword, custom_tags={}, custom_a
     psearch.search_single_kw(keyword)
     urls = psearch.get_urls_only_single(keyword)
     descriptions = psearch.get_descs_only_single(keyword)
-    scraper.scrape_multiple(urls, custom_tags=custom_tags, custom_attrs=custom_attrs, search_kw=search_kw, refer_google=refer_google)    
+    scraper.scrape_multiple(urls, custom_tags=custom_tags, custom_attrs=custom_attrs, get_kw=search_kw, google_refer=refer_google)    
     df = scraper.make_pandas_df(urls, descriptions)
 
     return df
@@ -18,7 +18,7 @@ def search_and_scrape_multiple(scraper, psearch, keywords, custom_tags={}, custo
     urls_summed = sum(urls, [])
     descriptions_summed = sum(descriptions, [])
 
-    scraper.scrape_multiple(urls_summed, custom_tags=custom_tags, custom_attrs=custom_attrs, search_kw=search_kw, refer_google=refer_google)    
+    scraper.scrape_multiple(urls_summed, custom_tags=custom_tags, custom_attrs=custom_attrs, get_kw=search_kw, google_refer=refer_google)    
     df = scraper.make_pandas_df(urls_summed, descriptions_summed)
 
     return df
